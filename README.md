e45e2b17556c6670eb0cf28ca895f3601746dedd
Nhat-Original <109066694+Nhat-Original@users.noreply.github.com>
2023-03-08 19:45:53 +0700

    final commit, fix the complete the output  template

--- README.md
+++ README.md
@@ -8,14 +8,31 @@ This simple c++ console program prints out a truth table based on the input bool
 
 1. Fork this repository.
 2. Clone your forked repository.
-3. Run the cpp_truth_table.exe file.
+3. Run the app.exe file.
 
 # Usage
 
-you can customize some feature in the customization.cpp file.
+You can customize some feature in the customization.cpp file.
+
+Making the project was so challenging that I decided to abandon it. What it only does is printing out the initial truth table of the input variables, it is unable to handle any logic.
+
+If you still want to run the program here is an example in the terminal:
+''
+$ ./app.exe
+Enter str or enter '?' to escape: a & b | -c
+=====================================
+| a | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
+=====================================
+| b | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |
+=====================================
+| c | 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |
+=====================================
+Enter str or enter '?' to escape: ?
+$
+''
+
+Even it fails, I have learnt that you should plan your project carefully and seperate your source code into different files.
 
 # Contact
 
 Hoang Minh Nhat - nhatoriginal@gmail.com
-
-# Acknowledgements
--- cpp_truth_table.cpp
+++ cpp_truth_table.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 #include "./customization.cpp"
 
@@ -23,6 +24,18 @@ void printArr(std::vector<char> arr)
     std::cout << arr[i] << " ";
   }
   std::cout << std::endl;
+}
+
+void printArr2d(std::vector<std::vector<int>> table, int tableSize)
+{
+  for (int i = 0; i < tableSize; ++i)
+  {
+    for (int j = 0; j < table[0].size(); ++j)
+    {
+      std::cout << table[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
 }
 
 void bubbleSort(std::vector<char> &arr)
@@ -65,8 +78,43 @@ void modifyVariablesArr(std::vector<char> &variablesArr, std::string str)
   addToVariablesArr(variablesArr, copy);
 }
 
-void addInitialVarsCols(std::vector<std::vector<int>> &table, int &col, std::vector<char> variablesArr)
+std::string toBinary(int n)
 {
+  std::string r;
+  while (n != 0)
+  {
+    r = (n % 2 == 0 ? "0" : "1") + r;
+    n /= 2;
+  }
+  return r;
+}
+
+void addInitialVarsCols(std::vector<std::vector<int>> &table, std::vector<char> variablesArr)
+{
+  int i = 0;
+  int stop = pow(2, variablesArr.size());
+
+  for (int i = 0; i < variablesArr.size(); ++i)
+  {
+    table[i].resize(stop);
+  }
+
+  while (i < stop)
+  {
+    std::string varSequence = toBinary(i);
+
+    while (varSequence.length() < variablesArr.size())
+    {
+      varSequence = '0' + varSequence;
+    }
+
+    for (int j = 0; j < variablesArr.size(); ++j)
+    {
+      table[j][i] = int(varSequence[j]) - 48;
+    }
+
+    ++i;
+  }
 }
 
 /**
@@ -92,10 +140,32 @@ std::string input()
   return str;
 }
 
-void output()
+void output(std::vector<std::vector<int>> table, int tableSize, std::vector<char> variablesArr)
 {
   if (out)
     return;
+
+  for (int j = 0; j < table[0].size() + 1; ++j)
+  {
+    std::cout << "====";
+  }
+  std::cout << "=" << std::endl;
+
+  for (int i = 0; i < tableSize; ++i)
+  {
+
+    std::cout << "| " << variablesArr[i] << " | ";
+    for (int j = 0; j < table[0].size(); ++j)
+    {
+      std::cout << table[i][j] << " | ";
+    }
+    std::cout << std::endl;
+    for (int j = 0; j < table[0].size() + 1; ++j)
+    {
+      std::cout << "====";
+    }
+    std::cout << "=" << std::endl;
+  }
 }
 
 void process(std::string str)
@@ -103,15 +173,16 @@ void process(std::string str)
   if (out)
     return;
 
-  int col = 0;
   std::vector<std::vector<int>> table;
+  table.resize(100);
 
   std::vector<char> variablesArr;
   modifyVariablesArr(variablesArr, str);
+  int tableSize = variablesArr.size();
 
-  addInitialVarsCols(table, col, variablesArr);
+  addInitialVarsCols(table, variablesArr);
 
-  output();
+  output(table, tableSize, variablesArr);
 }
 
 /**
